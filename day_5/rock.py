2682fdb925b698e6a7b73240aa1df840a28c3d6d
aleshch1991 <aleshch1991@gmail.com>
2022-01-14 13:39:27 -0800

    hw

---
+++ day_5/rock.py
@@ -0,0 +1,24 @@
+from random import randint as ran
+(ran(1,4))
+
+
+while True:
+    action = input("Rock, Papper, Sciccors?")
+    if action=="rock": 
+        continue
+        
+        
+    elif action =="sciccors": 
+        continue
+        
+        
+    elif action =="papper": 
+        continue    
+        
+        
+    elif action =="quit":
+        print("Your A Sore LOOSER GTFO")
+    break
+            
+               
+
